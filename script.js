fdb968986f999f20c6d9828710cd3d1b56708057
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-09-12 20:27:39 +0300

    add click sound effict for btns

--- script.js
+++ script.js
@@ -14,14 +14,19 @@ const productPopup = document.getElementById("productPopup");
 const popupTitle = document.getElementById("popupTitle");
 const popupClose = document.getElementById("popupClose");
 
-
 productItems.forEach((item) => {
     item.addEventListener("click", () => {
         popupTitle.textContent = item.textContent;
         productPopup.classList.add("open");
     });
 })
-
 popupClose.addEventListener("click", () => {
     productPopup.classList.remove("open");
 })
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        const sound = new Audio("click.mp3");
+        sound.play();
+    });
+});
