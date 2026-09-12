702073ad5867fc041f01ce2299999cca42d9c661
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-09-12 19:21:23 +0300

    popup

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     productsMenu.classList.remove("open");
 })
+
+const productItems = document.querySelectorAll(".product-item");
+const productPopup = document.getElementById("productPopup");
+const popupTitle = document.getElementById("popupTitle");
+const popupClose = document.getElementById("popupClose");
+
+
+productItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        popupTitle.textContent = item.textContent;
+        productPopup.classList.add("open");
+    });
+})
+
+popupClose.addEventListener("click", () => {
+    productPopup.classList.remove("open");
+})
